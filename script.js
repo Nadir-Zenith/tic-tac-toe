f88538337e3e06a3a05ddffb6d5ec868581fd820
Bijaya Khadka <zen.kalon@gmail.com>
2025-04-26 16:42:16 +0545

    tie script

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ function clickedField() {
     console.log("Clicked field 1");
     // mark selected field with an 'O'
     this.textContent = "O";
+    this.classList.remove("available");
+    this.removeEventListener("click", clickedField);
 
 
     // mark selected field with an 'O'
@@ -28,22 +30,33 @@ function clickedField() {
 
 
     // computer selects a random available field
-    const randomField = document.querySelector("#field5");
+    const availableFields = document.querySelectorAll(".field.available");
+    
+    const randomNumber = Math.floor(Math.random()*availableFields.length);
+    const randomField = availableFields[randomNumber];
 
 
     // if no field is available
-    // - end game: tie
+    if(!randomField){
+        // - end game: tie
+        console.log("end game in a tie");
+        document.querySelector("h2").textContent = "GAME OVER - it's a tie!";
+    }
+    else {
 
-    // mark selected field with an 'X'
-    randomField.textContent = 'X';
+        // mark selected field with an 'X'
+        randomField.textContent = 'X';
+        randomField.classList.remove("available");
+        randomField.removeEventListener("click", clickedField);
 
-    // check winner ... for X
-    // - rows: 1 & 2 & 3 or 4 & 5 & 6 or 7 & 8 & 9
-    // - columns: 1 & 4 & 7 or 2 & 5 & 8 or 3 & 6 & 9
-    // - diagonals: 1 & 5 & 9 or 3 & 5 & 7
+        // check winner ... for X
+        // - rows: 1 & 2 & 3 or 4 & 5 & 6 or 7 & 8 & 9
+        // - columns: 1 & 4 & 7 or 2 & 5 & 8 or 3 & 6 & 9
+        // - diagonals: 1 & 5 & 9 or 3 & 5 & 7
 
-    // if computer is winner:
-    // -end game: computer won!
+        // if computer is winner:
+        // -end game: computer won!
 
-    // repeat ...
+        // repeat ...
+    }
 }
